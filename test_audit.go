e9b16cccc2e2d6ed608f1286c38f4b4b1a16d206
aryanxgupta <itss.aryan@gmail.com>
2026-05-24 21:02:47 +0530

    demo push 2

--- test_audit.go
+++ test_audit.go
@@ -2,11 +2,12 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 )
 
 func GetUser(db *sql.DB, id string) {
-    query := fmt.Sprintf("SELECT * FROM users WHERE id = %s", id)
-    db.QueryRow(query)
+	query := fmt.Sprintf("SELECT * FROM users WHERE id = %s", id)
+	db.QueryRow(query)
+
 }
